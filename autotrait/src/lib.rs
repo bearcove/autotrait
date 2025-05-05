e2071ebe8ed36e4e45c564f41dd73147cf11d398
Amos Wenger <amos@bearcove.eu>
2025-05-05 20:35:39 +0200

    Port to quote

--- autotrait/src/lib.rs
+++ autotrait/src/lib.rs
@@ -1,6 +1,19 @@
 #![doc = include_str!("../README.md")]
 
+use quote::{TokenStreamExt as _, format_ident, quote};
 use unsynn::*;
+
+#[derive(Clone)]
+struct LifetimeName(pub Ident);
+
+impl quote::ToTokens for LifetimeName {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let punct = TokenTree::Punct(Punct::new('\'', Spacing::Joint));
+        let name = &self.0;
+        tokens.append(punct);
+        quote::ToTokens::to_tokens(&name, tokens);
+    }
+}
 
 keyword! {
     /// The "pub" keyword.
@@ -195,217 +208,247 @@ unsynn! {
     }
 }
 
-impl core::fmt::Display for Function {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "fn {}", self.name)?;
-        if let Some(generics) = &self.generics {
-            let mut first = true;
-            write!(f, "<")?;
-            for item in &generics.params.0 {
-                if !first {
-                    write!(f, ", ")?;
-                }
-                write!(f, "{}", item.value)?;
-                first = false;
-            }
-            write!(f, ">")?;
-        }
-        write!(f, "({})", self.params.content)?;
-        if let Some(ret) = &self.ret {
-            write!(f, " -> {}", ret.second)?;
-        }
-        write!(f, " {{ ... }}")
+impl quote::ToTokens for Function {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let name = &self.name;
+
+        let generics = if let Some(generics) = &self.generics {
+            let params = generics.params.0.iter().map(|param| &param.value);
+            quote! { < #(#params),* > }
+        } else {
+            quote! {}
+        };
+
+        let params = &self.params.content;
+
+        let return_type = if let Some(ret) = &self.ret {
+            let ret_type = &ret.second;
+            quote! { -> #ret_type }
+        } else {
+            quote! {}
+        };
+
+        quote::ToTokens::to_tokens(
+            &quote! {
+                fn #name #generics (#params) #return_type { ... }
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for Params {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let mut first = true;
-        for param in self.params.0.iter() {
-            if !first {
-                write!(f, ", ")?;
-            }
-            write!(f, "{}", param.value)?;
-            first = false;
-        }
-        Ok(())
+impl quote::ToTokens for Params {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let params = self.params.0.iter().map(|param| &param.value);
+        quote::ToTokens::to_tokens(
+            &quote! {
+                #(#params),*
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for TypeOrLifetime {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+impl quote::ToTokens for TypeOrLifetime {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
         match self {
-            TypeOrLifetime::Type(typ) => write!(f, "{}", typ),
-            TypeOrLifetime::Lifetime(lifetime) => write!(f, "'{}", &lifetime.ident),
+            TypeOrLifetime::Type(typ) => quote::ToTokens::to_tokens(&typ, tokens),
+            TypeOrLifetime::Lifetime(lifetime) => {
+                LifetimeName(lifetime.ident.clone()).to_tokens(tokens);
+            }
         }
     }
 }
 
-impl core::fmt::Display for Param {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+impl quote::ToTokens for Param {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
         match self {
             Param::ReceiverAndSelf(r) => {
-                write!(f, "&")?;
-                if let Some(lifetime) = &r.lifetime {
-                    write!(f, "'{} ", lifetime.ident)?;
-                }
-                if r._mut.is_some() {
-                    write!(f, "mut ")?;
-                }
-                write!(f, "self")
+                let lifetime = if let Some(lifetime) = &r.lifetime {
+                    let lifetime_token = LifetimeName(lifetime.ident.clone());
+                    quote! { #lifetime_token }
+                } else {
+                    quote! {}
+                };
+
+                let mutability = if r._mut.is_some() {
+                    quote! { mut }
+                } else {
+                    quote! {}
+                };
+
+                quote::ToTokens::to_tokens(&quote! { &#lifetime #mutability self }, tokens);
             }
-            Param::NamedParam(p) => write!(f, "{}: {}", p.ident, p.typ),
-        }
-    }
-}
-
-impl core::fmt::Display for SimpleType {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let path = self
-            .ident
-            .0
-            .iter()
-            .map(|ident| ident.value.to_string())
-            .collect::<Vec<_>>()
-            .join("::");
-        write!(f, "{}", path)
-    }
-}
-
-impl core::fmt::Display for DynTrait {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "dyn ")?;
-        let mut first = true;
-        for trait_type in &self.traits.0 {
-            if !first {
-                write!(f, " + ")?;
+            Param::NamedParam(p) => {
+                let ident = &p.ident;
+                let typ = &p.typ;
+                quote::ToTokens::to_tokens(&quote! { #ident: #typ }, tokens);
             }
-            write!(f, "{}", trait_type.value)?;
-            first = false;
         }
-        Ok(())
     }
 }
 
-impl core::fmt::Display for ImplTrait {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "impl ")?;
-        let mut first = true;
-        for trait_type in &self.traits.0 {
-            if !first {
-                write!(f, " + ")?;
-            }
-            write!(f, "{}", trait_type.value)?;
-            first = false;
-        }
-        Ok(())
+impl quote::ToTokens for SimpleType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let idents = self.ident.0.iter().map(|ident| &ident.value);
+        quote::ToTokens::to_tokens(
+            &quote! {
+                #(#idents)::*
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for Reference {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "&")?;
-        if let Some(lifetime) = &self.lifetime {
-            write!(f, "'{} ", lifetime.ident)?;
-        }
-        if self._mut.is_some() {
-            write!(f, "mut ")?;
-        }
-        write!(f, "{}", self.typ)
+impl quote::ToTokens for DynTrait {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let traits = self.traits.0.iter().map(|trait_type| &trait_type.value);
+        quote::ToTokens::to_tokens(
+            &quote! {
+                dyn #(#traits)+*
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for WithGenerics {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}", self.typ)?;
-        if !self.params.0.is_empty() {
-            write!(f, "<")?;
-            let mut first = true;
-            for param in &self.params.0 {
-                if !first {
-                    write!(f, ", ")?;
-                }
-                write!(f, "{}", param.value)?;
-                first = false;
-            }
-            write!(f, ">")?;
-        }
-        Ok(())
+impl quote::ToTokens for ImplTrait {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let traits = self.traits.0.iter().map(|trait_type| &trait_type.value);
+        quote::ToTokens::to_tokens(
+            &quote! {
+                impl #(#traits)+*
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for TupleType {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "(")?;
-        let mut first = true;
-        for typ in &self.types.content.0 {
-            if !first {
-                write!(f, ", ")?;
-            }
-            write!(f, "{}", typ.value)?;
-            first = false;
-        }
-        write!(f, ")")
+impl quote::ToTokens for Reference {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let lifetime = if let Some(lifetime) = &self.lifetime {
+            let lifetime_token = LifetimeName(lifetime.ident.clone());
+            quote! { #lifetime_token }
+        } else {
+            quote! {}
+        };
+
+        let mutability = if self._mut.is_some() {
+            quote! { mut }
+        } else {
+            quote! {}
+        };
+
+        let typ = &self.typ;
+
+        quote::ToTokens::to_tokens(
+            &quote! {
+                &#lifetime #mutability #typ
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for Slice {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "&")?;
-        if let Some(lifetime) = &self.lifetime {
-            write!(f, "'{} ", lifetime.ident)?;
+impl quote::ToTokens for WithGenerics {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let typ = &self.typ;
+
+        if self.params.0.is_empty() {
+            quote::ToTokens::to_tokens(&typ, tokens);
+        } else {
+            let params = self.params.0.iter().map(|param| &param.value);
+            quote::ToTokens::to_tokens(
+                &quote! {
+                    #typ < #(#params),* >
+                },
+                tokens,
+            );
         }
-        write!(f, "[{}]", self.element_type.content)
     }
 }
 
-impl core::fmt::Display for FnType {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match &self._fn {
-            FnTypeWord::CapitalFn(_fn_token) => write!(f, "Fn")?,
-            FnTypeWord::CapitalFnMut(_fn_mut_token) => write!(f, "FnMut")?,
-            FnTypeWord::CapitalFnOnce(_fn_once_token) => write!(f, "FnOnce")?,
-        }
-
-        write!(f, "(")?;
-        let mut first = true;
-        for param in &self.params.content.0 {
-            if !first {
-                write!(f, ", ")?;
-            }
-            write!(f, "{}", param.value)?;
-            first = false;
-        }
-        write!(f, ")")?;
-
-        if let Some(ret) = &self.ret {
-            write!(f, " -> {}", ret.second)?;
-        }
-
-        Ok(())
+impl quote::ToTokens for TupleType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let types = self.types.content.0.iter().map(|typ| &typ.value);
+        quote::ToTokens::to_tokens(
+            &quote! {
+                ( #(#types),* )
+            },
+            tokens,
+        );
     }
 }
 
-impl core::fmt::Display for Type {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+impl quote::ToTokens for Slice {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let lifetime = if let Some(lifetime) = &self.lifetime {
+            let lifetime_token = LifetimeName(lifetime.ident.clone());
+            quote! { #lifetime_token }
+        } else {
+            quote! {}
+        };
+
+        let element_type = &self.element_type.content;
+
+        quote::ToTokens::to_tokens(
+            &quote! {
+                &#lifetime [#element_type]
+            },
+            tokens,
+        );
+    }
+}
+
+impl quote::ToTokens for FnType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let fn_type = match &self._fn {
+            FnTypeWord::CapitalFn(_) => format_ident!("Fn"),
+            FnTypeWord::CapitalFnMut(_) => format_ident!("FnMut"),
+            FnTypeWord::CapitalFnOnce(_) => format_ident!("FnOnce"),
+        };
+
+        let params = self.params.content.0.iter().map(|param| &param.value);
+
+        let return_type = if let Some(ret) = &self.ret {
+            let ret_type = &ret.second;
+            quote! { -> #ret_type }
+        } else {
+            quote! {}
+        };
+
+        quote::ToTokens::to_tokens(
+            &quote! {
+                #fn_type ( #(#params),* ) #return_type
+            },
+            tokens,
+        );
+    }
+}
+
+impl quote::ToTokens for Type {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
         match self {
-            Type::Simple(simple) => write!(f, "{}", simple),
-            Type::Reference(reference) => write!(f, "{}", reference),
-            Type::WithGenerics(with_generics) => write!(f, "{}", with_generics),
-            Type::Tuple(tuple) => write!(f, "{}", tuple),
-            Type::Slice(slice) => write!(f, "{}", slice),
-            Type::Fn(fn_type) => write!(f, "{}", fn_type),
-            Type::DynTrait(dyn_trait) => write!(f, "{}", dyn_trait),
-            Type::ImplTrait(impl_trait) => write!(f, "{}", impl_trait),
+            Type::Simple(simple) => quote::ToTokens::to_tokens(simple, tokens),
+            Type::Reference(reference) => quote::ToTokens::to_tokens(reference, tokens),
+            Type::WithGenerics(with_generics) => quote::ToTokens::to_tokens(with_generics, tokens),
+            Type::Tuple(tuple) => quote::ToTokens::to_tokens(tuple, tokens),
+            Type::Slice(slice) => quote::ToTokens::to_tokens(slice, tokens),
+            Type::Fn(fn_type) => quote::ToTokens::to_tokens(fn_type, tokens),
+            Type::DynTrait(dyn_trait) => quote::ToTokens::to_tokens(dyn_trait, tokens),
+            Type::ImplTrait(impl_trait) => quote::ToTokens::to_tokens(impl_trait, tokens),
         }
     }
 }
 
-impl core::fmt::Display for GenericParam {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+impl quote::ToTokens for GenericParam {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
         match self {
-            GenericParam::Type(typ) => write!(f, "{}", typ),
-            GenericParam::Lifetime(lifetime) => write!(f, "'{}", &lifetime.ident),
+            GenericParam::Type(typ) => quote::ToTokens::to_tokens(&typ, tokens),
+            GenericParam::Lifetime(lifetime) => {
+                let lifetime_token = LifetimeName(lifetime.ident.clone());
+                lifetime_token.to_tokens(tokens);
+            }
         }
     }
 }
@@ -421,8 +464,6 @@ pub fn autotrait(
     let mut i = token_stream.to_token_iter();
     let b = i.parse::<ImplBlock>().unwrap();
 
-    use std::fmt::Write;
-    let mut code = String::new();
     let attr_bounds = TokenStream::from(attr)
         .to_token_iter()
         .parse::<AttrBounds>()
@@ -438,50 +479,62 @@ pub fn autotrait(
         }
     }
 
-    let bounds_str = if has_not_send {
-        ""
+    let bounds = if has_not_send {
+        quote! {}
     } else if has_not_sync {
-        ": Send"
+        quote! { : Send }
     } else {
-        ": Send + Sync"
+        quote! { : Send + Sync }
     };
 
-    let bounds = bounds_str;
-    for attr in &b.attrs {
-        writeln!(&mut code, "#{}", attr.group.tokens_to_string()).unwrap();
-    }
-    write!(&mut code, "pub trait {}{} {{", b.trait_name, bounds).unwrap();
-    // Add function declarations to the trait based on functions in the implementation
-    for f in &b.body.content {
-        if f._async.is_some() {
-            write!(&mut code, "async ").unwrap();
-        }
-        write!(&mut code, "fn {}", f.name).unwrap();
-        // Write generics if they exist
-        if let Some(generics) = &f.generics {
-            write!(&mut code, "<").unwrap();
-            let mut first = true;
-            for item in &generics.params.0 {
-                if !first {
-                    write!(&mut code, ", ").unwrap();
-                }
-                write!(&mut code, "{}", item.value).unwrap();
-                first = false;
-            }
-            write!(&mut code, ">").unwrap();
-        }
-        write!(&mut code, "({})", f.params.content).unwrap();
+    let trait_name = &b.trait_name;
+    let attrs = b.attrs.iter().map(|attr| {
+        let attr_content = attr.group.tokens_to_string();
+        quote! { #[#attr_content] }
+    });
 
-        // Handle return type
-        if let Some(ret) = &f.ret {
-            write!(&mut code, " -> {}", ret.second).unwrap();
+    let functions = b.body.content.iter().map(|f| {
+        let async_kw = if f._async.is_some() {
+            quote! { async }
+        } else {
+            quote! {}
+        };
+
+        let fn_name = &f.name;
+
+        let generics = if let Some(generics) = &f.generics {
+            let params = generics.params.0.iter().map(|param| &param.value);
+            quote! { < #(#params),* > }
+        } else {
+            quote! {}
+        };
+
+        let params = &f.params.content;
+
+        let return_type = if let Some(ret) = &f.ret {
+            let ret_type = &ret.second;
+            quote! { -> #ret_type }
+        } else {
+            quote! {}
+        };
+
+        quote! {
+            #async_kw fn #fn_name #generics (#params) #return_type;
         }
+    });
 
-        write!(&mut code, ";").unwrap();
-    }
-    write!(&mut code, "}}").unwrap();
+    let trait_def = quote! {
+        #(#attrs)*
+        pub trait #trait_name #bounds {
+            #(#functions)*
+        }
+    };
 
-    let mut output_stream = code.into_token_stream();
-    output_stream.extend(TokenStream::from(item_clone));
-    output_stream.into()
+    let mut output = TokenStream::new();
+    trait_def.to_tokens(&mut output);
+
+    let item_ts: TokenStream = item_clone.into();
+    output.extend(item_ts);
+
+    output.into()
 }
